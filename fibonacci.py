6f68b48daaa51b9cfb8a83a22b60e45109754530
sristy <45727820+beertocode@users.noreply.github.com>
2018-12-16 17:25:35 +0530

    Add files via upload
    fibonacci series

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+n=int(raw_input("enter limit:"))
+f0=0
+f1=1
+s=0
+
+if n==1:
+    print f0
+else:
+    while s<n:
+        print f0
+        f=f0+f1
+        
+        f0=f1
+        f1=f
+        s+=1
